a28420ae140de0e4b68469bb1d682a62892d9dc9
Фикс <149292791+fiksofficial@users.noreply.github.com>
2025-05-24 10:46:36 +0500

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -2,32 +2,33 @@
 # https://github.com/all-licenses/GNU-General-Public-License-v3.0
 
 # meta developer: @pymodule
-# requires: pyspeedtest
+# requires: speedtest-cli
 
-import pyspeedtest
+import speedtest
 from .. import loader
 
 class SpeedTestMod(loader.Module):
-    """Модуль для проверки скорости интернета через pyspeedtest"""
+    """Модуль для проверки скорости интернета"""
 
     strings = {"name": "SpeedTest"}
 
     async def speedcmd(self, message):
         """Запускает тест скорости интернета"""
-        msg = await message.edit("Запускаем Speedtest... 🏎️")
+        msg = await message.edit("Запускаем Speedtest... 🏁")
 
         try:
-            st = pyspeedtest.SpeedTest()
-            ping = st.ping()
-            download_speed = st.download() / 1_000_000
-            upload_speed = st.upload() / 1_000_000
+            st = speedtest.Speedtest()
+            st.get_best_server()
+            download = st.download() / 1_000_000  # Мбит/с
+            upload = st.upload() / 1_000_000      # Мбит/с
+            ping = st.results.ping
 
             await msg.edit(
                 f"<emoji document_id=5325547803936572038>✨</emoji> <b>Speedtest завершён!</b> <emoji document_id=5325547803936572038>✨</emoji>\n\n"
                 f"<b>Ping:</b> <i>{ping:.2f} ms</i>\n"
-                f"<emoji document_id=6041730074376410123>📥</emoji> <b>Загрузка:</b> <i>{download_speed:.2f} Mbps</i>\n"
-                f"<emoji document_id=6041730074376410123>📤</emoji> <b>Отдача:</b> <i>{upload_speed:.2f} Mbps</i>",
+                f"<emoji document_id=6041730074376410123>📥</emoji> <b>Загрузка:</b> <i>{download:.2f} Mbps</i>\n"
+                f"<emoji document_id=6041730074376410123>📤</emoji> <b>Отдача:</b> <i>{upload:.2f} Mbps</i>",
                 parse_mode="HTML"
             )
         except Exception as e:
-            await msg.edit(f"<b>Ошибка при выполнении speedtest:</b> <code>{e}</code>")
+            await msg.edit(f"<b>Ошибка при выполнении Speedtest:</b>\n<code>{e}</code>")
